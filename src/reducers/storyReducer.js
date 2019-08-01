4be7e5f65c5a3eabe1cefe0db69e07a0408b773d
github username <github email address>
2019-08-01 16:48:29 -0400

    set up conditions for storyReducer

---
+++ src/reducers/storyReducer.js
@@ -0,0 +1,16 @@
+export default (state={stories: [], loading: false}, action) => {
+  switch(action.type) {
+
+    case 'LOADING_STORIES':
+      return {
+        ...state, loading:true
+      };
+
+    case 'FETCHING_STORIES':
+      return {
+        stories: [...state.stories, action.payload], loading: false
+      }
+    default:
+      return state;
+  }
+}

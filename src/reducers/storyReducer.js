dbb7f81b70c2d90de8e4cfb296ace863cb826a1b
github username <github email address>
2019-08-01 18:54:59 -0400

    formate update

--- src/reducers/storyReducer.js
+++ src/reducers/storyReducer.js
@@ -2,14 +2,11 @@ export default (state={stories: [], loading: false}, action) => {
   switch(action.type) {
 
     case 'LOADING_STORIES':
-      return {
-        ...state, loading:true
-      };
+      return {...state, loading:true};
 
     case 'FETCHING_STORIES':
-      return {
-        stories: [...state.stories, action.payload], loading: false
-      }
+      return {stories: [...state.stories, action.payload], loading: false}
+
     default:
       return state;
   }

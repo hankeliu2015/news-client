6f4c7b47ad23c2180788f134a3eddadd8aff895d
github username <github email address>
2019-08-01 14:05:16 -0400

    change to class syntax, add render() function in return. Import react component and connect. Create mapStatetoProps constance.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+
+class App extends Component {
+  render () {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+          </a>
+        </header>
+
+        <hr></hr>
+        <h4>Welcome to React Redux Job News</h4>
+
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStatetoProps = state => {
+  return {
+    // stories: state.storyReducer.stories
+  }
+}
+
+export default connect(mapStatetoProps)(App)

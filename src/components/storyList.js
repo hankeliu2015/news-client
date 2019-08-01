da2e4639b3b37d1f17823e4dc478bc8edbd725e9
github username <github email address>
2019-08-01 17:40:29 -0400

    add stories iteration to component

--- src/components/storyList.js
+++ src/components/storyList.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react'
 
 class StoryList extends Component {
   render() {
+    const list = this.props.stories.map((story, index) => <li key={index}>{story.id}: {story.title} </li>)
+
     return (
-      <p>Hacker News Jobs Stroies display here</p>
+      <div>
+        <p>Hacker News Jobs Stroies display here</p>
+        {list}
+      </div>
     )
-}
+  }
 }
 
 export default StoryList

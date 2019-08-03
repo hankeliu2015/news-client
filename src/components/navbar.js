ffdccb91b3e3651ac53108d5b2f2c01033f3593b
github username <github email address>
2019-08-03 17:05:37 -0400

    add stype for Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 
 export class Navbar extends Component {
+
   render() {
+
+    const link = {
+      width: '100px',
+      padding: '12px',
+      margin: '0 6px 6px',
+      background: 'blue',
+      textDecoration: 'none',
+      color: 'white',
+    }
     return (
       <div>
-      <NavLink to="/news">News</NavLink>
-      <NavLink to="/story">Story</NavLink>
+      <NavLink to="/news" style={link} activeStyle={{background: 'darkblue'}}>News</NavLink>
+      <NavLink to="/story" style={link} activeStyle={{background: 'darkblue'}}>Story</NavLink>
       </div>
     )
   }

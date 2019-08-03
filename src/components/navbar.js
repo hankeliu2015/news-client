5ff1e99ff736deaa69ec141be8bd4ae292a4e5be
github username <github email address>
2019-08-03 16:51:36 -0400

    change navbar component to class syntax

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom'
 
-export const Navbar = () => {
-  return (
-    <div>
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
       <NavLink to="/news">News</NavLink>
       <NavLink to="/story">Story</NavLink>
-    </div>
-  )
+      </div>
+    )
+  }
 }

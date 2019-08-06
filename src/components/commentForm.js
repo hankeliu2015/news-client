701ace898015d98188bb6cdc5903cc00988db220
github username <github email address>
2019-08-06 11:39:16 -0400

    bind functions for input field and form - wip

--- src/components/commentForm.js
+++ src/components/commentForm.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react'
 
 class Comment extends Component {
 
+  state = {
+    comment: ""
+  }
+
+  handleOnChange = event => {
+    // debugger
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    //pass the state.comments to Rails API
+  }
+
   render() {
 
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleOnSubmit}>
           <label>Please add your comment</label>
           <br></br>
-          <textarea></textarea>
+          <textarea value={this.state.comment} name="comment" onChange={this.handleOnChange}></textarea>
           <br></br>
           <button type="submit">Submit</button>
         </form>

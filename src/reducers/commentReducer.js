dceaa0a1a82198149abf908c57ed3e3d22eabca7
github username <github email address>
2019-08-06 22:51:15 -0400

    add uploading comment and fetching comment condition to commentAdction.

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -2,8 +2,10 @@ export default function commentReducer(state={comments: [], loading: false}, act
   switch(action.type) {
 
     case 'UPLOADING_COMMENT':
+      return {...state, loading:true};
 
     case 'FETCHING_COMMENT':
+      return {comments: [...state.comments, action.payload], loading: false}
 
     default:
       return state;

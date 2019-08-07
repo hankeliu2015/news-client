137bc921136bf85e13f8e6bb51dc5ae0c0f61879
github username <github email address>
2019-08-06 20:59:20 -0400

    wip - commentAction json response

--- src/actions/commentAction.js
+++ src/actions/commentAction.js
@@ -1,9 +1,10 @@
 export function commentUpload(storyComment) {
-
+  // let commentData = storyComment.serializeArray();
+  // debugger
   return dispatch => {
     dispatch({type: 'UPLOADING_COMMENT'});
     return (
-      fetch('/comments', {
+      fetch('https://localhost:3001/comments.json', {
 
       method: 'POST',
       headers: {
@@ -11,10 +12,16 @@ export function commentUpload(storyComment) {
         'Accept': 'application/json',
         // 'X-CSRF-Token': valueCSRF
       },
-      body: JSON.stringify(storyComment) //? need pass in the bodydate
-      //credentials: 'same-origin'  //might not need this.
+      body: JSON.stringify(storyComment),
+      credentials: 'same-origin'  //might not need this.
 
       })
+      .then(resp => resp.json())
+      .then(comment => {
+        dispatch({type:'FETCHING_COMMENT', payload: comment})
+      })
+      .catch(function(error) {console.log('There has been a problem with your fetch operation: ', error.message);})
+
       // .then(resp => reap.json()) // use this to update redux state.
     )
   }

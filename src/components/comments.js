e3896739bdd4fcb45da1c7c4d2d180cbdcafeb9a
github username <github email address>
2019-08-06 23:22:59 -0400

    wip - dispaly comments for a story from Redux state

--- src/components/comments.js
+++ src/components/comments.js
@@ -5,7 +5,11 @@ class Comments extends Component {
 
   render(){
     return (
-      <p>Display the comments about a single story</p>
+      <div>
+        <h3>Comments</h3>
+        <p>Display the comments about a single story from Redux State or Database? </p>
+
+      </div>
     )
   }
 }

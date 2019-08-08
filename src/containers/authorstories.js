68d2438e309393f2b8b23109ff2e756c7abe6a94
github username <github email address>
2019-08-08 19:31:04 -0400

    created form to submit author's own story.

--- src/containers/authorstories.js
+++ src/containers/authorstories.js
@@ -6,6 +6,18 @@ class AuthorStories extends Component {
     return (
       <div>
         <h4>Publishing your own story: </h4>
+        <form>
+          <label>Story Content</label>
+          <br></br>
+          <textarea rows="5" cols="60"></textarea>
+          <br></br>
+          <label>Author</label>
+          <br></br>
+          <input type="text"></input>
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+
       </div>
     )
   }

772c0abb113a8afdec3faf79980b3d0646c83f0e
github username <github email address>
2019-08-09 14:55:57 -0400

    add authorStories to seperate from stories fetch from external sources. add spead operator to state to ensure authorStories not overwrite other element saved in states after fething.

--- src/reducers/storyReducer.js
+++ src/reducers/storyReducer.js
@@ -1,17 +1,17 @@
-export default function storyReducer(state={stories: [], loading: false}, action) {
+export default function storyReducer(state={authorStories:[], stories: [], loading: false}, action) {
   switch(action.type) {
 
     case 'LOADING_STORIES':
       return {...state, loading:true};
 
     case 'FETCHING_STORIES':
-      return {stories: [...state.stories, action.payload], loading: false}
+      return {...state, stories: [...state.stories, action.payload], loading: false}
 
     case 'LOADING_AUTHORCREATEDSTORY':
       return {...state, loading:true};
 
     case 'ADDING_AUTHORCREATEDSTORY':
-      return {story: action.payload, loading:false}
+      return {...state, authorStories: [...state.authorStories, action.payload], loading:false}
 
     default:
       return state;

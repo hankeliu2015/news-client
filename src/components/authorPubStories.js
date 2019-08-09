5e04f6ea60cbb254bb9dd27f43c18177d2822db9
github username <github email address>
2019-08-09 16:51:07 -0400

    display author created story

--- src/components/authorPubStories.js
+++ src/components/authorPubStories.js
@@ -3,9 +3,13 @@ import { connect } from 'react-redux'
 
 class AuthorPubStories extends Component {
   render() {
+
+    const authorStories = this.props.authorStories.map((story, index) => <li key={index}>{story.story_content} Created at: {story.created_at}</li>)
+
     return (
       <div>
-        <h4>List all author published stories:</h4>
+        <h4>Please see below your story published:</h4>
+        {authorStories}
 
       </div>
     )

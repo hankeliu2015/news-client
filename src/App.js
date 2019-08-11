538f4444295f9e8ef64832b40c0a8955eca12025
github username <github email address>
2019-08-10 20:26:39 -0400

    clean up comments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
-// import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import { connect } from 'react-redux';
 import StoryList from './components/storyList';
-// import { fetchStories } from './actions/storyAction'
 
 import {BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import StoryComments from './containers/storycomments';
@@ -16,12 +14,7 @@ import Container from 'react-bootstrap/Container'
 
 class App extends Component {
 
-  // componentDidMount() {
-  //   this.props.fetchStories()
-  // }
-
   render () {
-    // debugger
     return (
       <div>
         <Router>
@@ -41,12 +34,3 @@ class App extends Component {
 }
 
 export default App
-
-// const mapStatetoProps = state => {
-//   return {
-//     stories: state.storyReducer.stories,
-//     loading: state.storyReducer.loading
-//   }
-// }
-//
-// export default connect(mapStatetoProps, {fetchStories})(App)

2df2b3229af48faa8f2dc80a88308e5029cc1c7f
github username <github email address>
2019-08-12 21:17:31 -0400

    testing change authorCreateStories to functional component

--- src/components/authorCreatedStories.js
+++ src/components/authorCreatedStories.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-class AuthorCreatedStories extends Component {
 
-  render() {
+export default function AuthorCreatedStories(props) {
 
     return (
       <div>
         <h4>Your story is successfully published:</h4>
-        {this.props.authorStories}
+        {props.authorStories}
 
         <h5>Please check other authors published story:</h5>
-        {this.props.allAuthorsStories}
+        {props.allAuthorsStories}
 
       </div>
     )
-  }
 }
 
-export default AuthorCreatedStories
+// class AuthorCreatedStories extends Component {
+//
+//   render() {
+//
+//     return (
+//       <div>
+//         <h4>Your story is successfully published:</h4>
+//         {this.props.authorStories}
+//
+//         <h5>Please check other authors published story:</h5>
+//         {this.props.allAuthorsStories}
+//
+//       </div>
+//     )
+//   }
+// }
+//
+// export default AuthorCreatedStories

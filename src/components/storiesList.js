18646b4ca9689d44659e1302387a7a13a1fd3dc7
github username <github email address>
2019-08-13 15:51:43 -0400

    clean up comments and format

--- src/components/storiesList.js
+++ src/components/storiesList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function StoriesList(props) {
-  // debugger
   return (
     <div>
       <h4 style={{ borderTop: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>News Feed from Hacker News </h4>

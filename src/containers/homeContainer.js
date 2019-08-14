916d8de38318a82e8f7495a59c33b5660bf89b6e
github username <github email address>
2019-08-14 15:03:36 -0400

    wip - testing set a user name inside cookies objext, use deconstructor to get cookies name

--- src/containers/homeContainer.js
+++ src/containers/homeContainer.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Home extends Component {
   render() {
-    debugger
+
+    const { cookies } = this.props;
+
+    const userName = cookies.get('name')
+    // debugger
     return (
       <div>
         <h3>Home Page for Cookies Testing</h3>
+        <p>Current User: {userName}</p>
       </div>
     )
   }
 }
 
-export default Home
+const mapStateToProps = (state, ownProps) => {
+  return ({
+    state: state,
+    cookies: ownProps.cookies
+  })
+}
+export default connect(mapStateToProps)(Home)
